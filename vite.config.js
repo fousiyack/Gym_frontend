f7916129f3807ef7339ecf78f04e26988ceaaa63
fouzi <fousiyack64@gmail.com>
2023-09-12 17:47:00 +0530

    VITE CHANGE

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const isProduction = process.env.NODE_ENV === "production";
+const BASE_URL = isProduction
+  ? "https://be-strong-fitness.onrender.com"
+  : "http://localhost:8000"; // Update with your local backend URL
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  define: {
+    "process.env.BASE_URL": JSON.stringify(BASE_URL),
   },
 });
